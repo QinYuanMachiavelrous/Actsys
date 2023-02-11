2656e85719ba1974bf34146aa5ebe93b799c08ce
QinYuanMachiavelrous <IshidaHachikou@gmail.com>
2023-02-11 13:30:24 +0800

    First commit

---
+++ highestPair/highestPair/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace highestPair
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            highestPairClass highestPairClass = new highestPairClass();
+
+            highestPairClass.highestPair(new string[] { "A", "A", "Q", "Q", "6" });
+
+            highestPairClass.highestPair(new string[] { "J", "6", "3", "10", "8" });
+
+            highestPairClass.highestPair(new string[] { "K", "7", "3", "9", "3" });
+
+            highestPairClass.highestPair(new string[] { "K", "9", "10", "J", "Q" });
+
+            highestPairClass.highestPair(new string[] { "3", "5", "5", "5", "5" });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ highestPair/highestPair/highestPairClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace highestPair
+{
+    public class highestPairClass
+    {
+        public object results(string paired)
+        {
+            if (paired == "")
+            {
+                Console.WriteLine(false);
+
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("[true, \"{0}\"]", paired);
+
+                return new object[] { true, paired };
+            }
+        }
+
+        public string paired(string[] array)
+        {
+            bool boolean = false;
+            string paired = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (boolean == true)
+                {
+                    break;
+                }
+                else
+                {
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[i] == array[j])
+                        {
+                            paired = array[i];
+                            boolean = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            return paired;
+        }
+
+        public object highestPair(string[] array)
+        {
+            string pair = paired(array);
+
+            object result = results(pair);
+
+            return result;
+        }
+    }
+}
